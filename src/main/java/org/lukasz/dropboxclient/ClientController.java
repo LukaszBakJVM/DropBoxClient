fec262e5aebddf3e464d13cd030ec408a7a24f56
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-04-25 16:53:08 +0200

    show all files from server

--- src/main/java/org/lukasz/dropboxclient/ClientController.java
+++ src/main/java/org/lukasz/dropboxclient/ClientController.java
@@ -1,11 +1,9 @@
 package org.lukasz.dropboxclient;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -20,6 +18,12 @@ public class ClientController {
     @ResponseStatus(HttpStatus.CREATED)
     Mono<Void> upload(@RequestParam("file") MultipartFile file) {
         return services.sendFile(file);
+    }
+
+    @GetMapping("/files")
+    @ResponseStatus(HttpStatus.OK)
+    Flux<String> allFiles() {
+        return services.showAllFiles();
     }
 }
 
