4f8bf65c379e4c00241ebc80adaa247ea540ed38
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-04-25 12:38:18 +0200

    DropBox Client

---
+++ src/main/java/org/lukasz/dropboxclient/DropBoxClientApplication.java
@@ -0,0 +1,13 @@
+package org.lukasz.dropboxclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropBoxClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropBoxClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lukasz/dropboxclient/DropBoxClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lukasz.dropboxclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropBoxClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
